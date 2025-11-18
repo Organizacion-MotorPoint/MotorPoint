04529bc568f7bc563e67fbe8a75a06019ab4eb98
AshleeGarcia <ashleegarcia.dev@gmail.com>
2025-11-18 00:09:34 -0500

    feat: agregar componente HomePage con secciones de héroe, beneficios, categorías y estadísticas

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import Benefits from "../components/Benefits";
+import Categories from "../components/Categories";
+import Stats from "../components/Stats";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Benefits />
+      <Categories />
+      <Stats />
+    </>
+  );
+}
+
+export default HomePage;
