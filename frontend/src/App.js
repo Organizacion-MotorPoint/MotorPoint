b26cfff9dde1964ace48b58a4c3647d33889386d
AshleeGarcia <ashleegarcia.dev@gmail.com>
2025-11-18 00:10:49 -0500

    feat: reemplazar contenido de la ruta principal con el componente HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -11,21 +12,9 @@ function App() {
       <Navbar />
 
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        <Route
-          path="/"
-          element={
-            <div style={{ textAlign: "center", padding: "2rem" }}>
-              <h1>Bienvenido a MotorPoint</h1>
-              <p>
-                <Link to="/login">Ir a Login</Link> |{" "}
-                <Link to="/register">Ir a Registro</Link>
-              </p>
-            </div>
-          }
-        />
       </Routes>
 
       <Footer />
